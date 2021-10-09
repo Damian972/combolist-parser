0e3553562f3c1169676edb5669126213eab50641
Damian972 <25445518+Damian972@users.noreply.github.com>
2021-10-09 12:39:08 -0400

    v2.0

--- README.md
+++ README.md
@@ -1,5 +1,20 @@
-combolist-parser
+## combolist-parser
+
 #### Usage:
-```BASH 
-php combolist-parser.php list.txt
+
+```BASH
+php combolist-parser -f list.txt
 ```
+
+- Supported options:
+  ````bash
+  -f <file>       File to parse
+  -h, --help      Show help
+  -l, --logs      Save errors logs
+  -u, --unknow    Save unknow results
+  ````
+
+#### What's new 🎉️
+
+- The progression is now displayed
+- Decide if you want logs, unknow results by passing arguments
--- combolist-parser.php
+++ combolist-parser.php
@@ -4,64 +4,161 @@
      * Combolist parser
      * Author: Damian972
      * Description: A simple script to filter gmail, yahoo, hotmail, orange, laposte, etc... mail with combolist like email:password
-     * Version: 1.0
-     * Usage: php combolist-parser.php list.txt
+     * Version: 2.0
+     * Usage: php combolist-parser.php -f list.txt.
      */
-    
-    define('ROOT', __DIR__);
-    define('RESULTS_FOLDER', ROOT.DIRECTORY_SEPARATOR.'results'.DIRECTORY_SEPARATOR);
     ini_set('memory_limit', '800M');
+    define('RESULTS_FOLDER', __DIR__.DIRECTORY_SEPARATOR.'results');
+    define('EMAIL_PATTERN_VALIDATOR', '/^[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+$/');
+    define('LOGS_FILENAME', 'logs');
+    define('LOGS_TEMPLATE', '[{DATETIME}] {MESSAGE} - {TARGET_FILE}');
 
-    $filter = array(
+    $filters = [
         'gmail' => 'gmail.com',
         'yahoo' => ['yahoo.com', 'yahoo.fr'],
         'hotmail' => 'hotmail.com',
         'orange' => 'orange.fr',
-        'laposte' => 'laposte.net'
-    );
+        'laposte' => 'laposte.net',
+    ];
 
-    if($argc === 2){
-        is_dir(RESULTS_FOLDER) || mkdir(RESULTS_FOLDER);
-        $file = $argv[1];
-        if(is_readable($file)){
-            $handle = fopen($file, 'r');
-            if ($handle){
-                $total_lines = count_lines($file);
-                echo 'Welcome in Combolist parser'.PHP_EOL;
-                echo '[!] Total lines to parse in '.$file.': '.$total_lines.PHP_EOL;
-                while (($line = fgets($handle)) !== false){
-                    if (!empty($line)){
-                        $is_unknow = true;
-                        foreach ($filter as $key => $value){
-                            $splitted_line = explode('@', trim($line));
-                            $tmp_value = explode(':', $splitted_line[1]);
-                            if (is_array($value)){
-                                for ($i = 0; $i < count($value); $i++){
-                                    if ($tmp_value[0] === $value[$i]){
-                                        file_put_contents(RESULTS_FOLDER.$key.'.txt', trim($line).PHP_EOL, FILE_APPEND | LOCK_EX);
-                                        $is_unknow = false;
-                                    }
-                                }
-                            }else{
-                                if ($tmp_value[0] === $value){
-                                    file_put_contents(RESULTS_FOLDER.$key.'.txt', trim($line).PHP_EOL, FILE_APPEND | LOCK_EX);
-                                    $is_unknow = false;
-                                }
-                            }
-                        }
-                        if($is_unknow) file_put_contents(RESULTS_FOLDER.'unknow.txt', trim($line).PHP_EOL, FILE_APPEND | LOCK_EX);
-                    }
+    $options = getopt('f:hu', ['help', 'unknow', 'logs']);
+    if (empty($options['f']) || isset($options['h']) || isset($options['help'])) {
+        showHelp();
+
+        exit;
+    }
+
+    $file = $options['f'];
+    if (!is_readable($file)) {
+        echo '[-] File not found or readable'.PHP_EOL;
+
+        exit;
+    }
+
+    $linesCount = getLinesCountForFile($file);
+    if (0 === $linesCount) {
+        echo '[-] File is empty'.PHP_EOL;
+
+        exit;
+    }
+
+    is_dir(RESULTS_FOLDER) || mkdir(RESULTS_FOLDER);
+    $handle = fopen($file, 'r');
+    if ($handle) {
+        $withUnknow = isset($options['u']) || isset($options['unknow']);
+        $canLog = isset($options['logs']);
+
+        $cursorIndex = 0;
+        $progressInPercent = 0;
+        while (($line = fgets($handle)) !== false) {
+            $consoleOutput = '';
+            $line = strtolower(trim($line));
+            if (empty($line)) {
+                ++$cursorIndex;
+
+                continue;
+            }
+            $parts = explode(':', $line);
+            if (2 > count($parts)) {
+                if ($canLog) {
+                    appendToLogs("Invalid line {$cursorIndex}", $file);
+                }
+                ++$cursorIndex;
+
+                continue;
+            }
+
+            $email = $parts[0];
+            // implode + array_slice to prevent password with ':' in it
+            $password = implode('', array_slice($parts, 1));
+            if (!preg_match(EMAIL_PATTERN_VALIDATOR, $email)) {
+                if ($canLog) {
+                    appendToLogs("Invalid email at line {$cursorIndex}", $file);
+                }
+                ++$cursorIndex;
+
+                continue;
+            }
+
+            [$identifier, $emailDomain] = explode('@', $email);
+
+            foreach ($filters as $serviceName => $domains) {
+                $domains = is_array($domains) ? $domains : [$domains];
+                if (in_array($emailDomain, $domains, true)) {
+                    appendToFile($serviceName, "{$email}:{$password}");
+
+                    break;
                 }
             }
-			fclose($handle);
-        }else { echo '[-] File not found'.PHP_EOL; }
-    }else { echo '[!] Usage: php combolist-parser.php list.txt'.PHP_EOL; }
 
-    function count_lines(string $file){
-        $total_lines = 0;
-        if (file_exists($file)){
-            $handle = fopen($file, "r");
-            while (!feof($handle)){ if (fgets($handle) !== false) $total_lines += 1; }
-            fclose($handle);
-        } return $total_lines;
+            if ($withUnknow) {
+                appendToFile('unknow', "{$email}:{$password}");
+            }
+
+            ++$cursorIndex;
+            $progressInPercent = round(($cursorIndex / $linesCount) * 100, 2);
+
+            $consoleOutput .= "Progress: {$cursorIndex}/{$linesCount} ({$progressInPercent}%)  ".PHP_EOL;
+
+            replaceOut($consoleOutput);
+        }
+    }
+    echo PHP_EOL;
+
+    function getLinesCountForFile(string $file): int
+    {
+        $linesCount = 0;
+        $handle = fopen($file, 'r');
+        while (!feof($handle)) {
+            if (false !== fgets($handle)) {
+                ++$linesCount;
+            }
+        }
+        fclose($handle);
+
+        return $linesCount;
+    }
+
+    function appendToFile(string $filename, string $value, bool $appendMode = true): void
+    {
+        $flags = $appendMode ? FILE_APPEND | LOCK_EX : LOCK_EX;
+        file_put_contents(RESULTS_FOLDER.DIRECTORY_SEPARATOR."{$filename}.txt", $value.PHP_EOL, $flags);
+    }
+
+    function appendToLogs(string $message, string $targetFile, bool $appendMode = true): void
+    {
+        $message = str_replace(
+            '{DATETIME}',
+            date('Y-m-d H:i:s'),
+            str_replace(
+                '{MESSAGE}',
+                $message,
+                str_replace(
+                    '{TARGET_FILE}',
+                    $targetFile,
+                    LOGS_TEMPLATE
+                )
+            )
+        );
+
+        appendToFile(LOGS_FILENAME, $message, $appendMode);
+    }
+
+    function replaceOut(string $value): void
+    {
+        $linesCount = substr_count($value, PHP_EOL);
+        echo chr(27).'[0G';
+        echo $value;
+        echo chr(27).'['.($linesCount).'A';
+    }
+
+    function showHelp(): void
+    {
+        $split = explode(DIRECTORY_SEPARATOR, __FILE__);
+        echo 'Usage: php '.end($split).' -f list.txt'.PHP_EOL;
+        echo 'Options:'.PHP_EOL;
+        echo '  -f <file>       File to parse'.PHP_EOL;
+        echo '  -h, --help      Show help'.PHP_EOL;
+        echo '  -l, --logs      Save errors logs'.PHP_EOL;
+        echo '  -u, --unknow    Save unknow results'.PHP_EOL;
     }
